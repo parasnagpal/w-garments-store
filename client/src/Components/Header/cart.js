7d25a751db9c6b2af07ccadc7dbb5027dde1a1bf
Paras Nagpal <parasnagpal05@gmail.com>
2021-06-22 03:17:46 +0530

    Component Cart

---
+++ client/src/Components/Header/cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {IoCartOutline} from 'react-icons/io5'
+
+class Cart extends React.Component{
+    render(){
+        return(
+            <div>
+                <IoCartOutline/>
+            </div>
+        );
+    }
+};
+
+export default Cart;
